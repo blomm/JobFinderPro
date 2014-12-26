4ab8d67f24de5dcbfb04f0b678c48cf3fb938bfc
michael blom <blomster@me.com>
2014-12-26 04:50:31 +0000

    now uses ng-resource

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,12 @@
-angular.module('myApp', []);
+angular.module('myApp', ['ngResource']);
 
-angular.module('myApp').controller('myController', function($scope) {
-    $scope.jobs = [{
-        title: 'sales manager',
-        description: 'you will sell stuff'
-    }, {
-        title: 'manager',
-        description: 'you will manage stuff'
-    }];
+angular.module('myApp').controller('myController', function($scope, $resource) {
+    // $scope.jobs = [{
+    //     title: 'sales manager',
+    //     description: 'you will sell stuff'
+    // }, {
+    //     title: 'manager',
+    //     description: 'you will manage stuff'
+    // }];
+    $scope.jobs = $resource('/api/jobs').query();
 })
